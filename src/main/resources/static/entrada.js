706caf936b20ecf14f1b302d821eba9cf4a34b4b
David Esteban Useche <david.useche@mail.escuelaing.edu.co>
2016-10-30 20:57:54 -0500

    Nombre solucionado, haciendoEntrarAProyecto

--- src/main/resources/static/entrada.js
+++ src/main/resources/static/entrada.js
@@ -1,5 +1,44 @@
+function connect() {
+    $("#connect").prop("disabled", true);
+    $("#disconnect").prop("disabled", false);
+    var socket = new SockJS('/stompendpoint');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/waiting.'+sessionStorage.name, function (data) {
+            var theObject = JSON.parse(data.body);
+            var irAProyecto = confirm(theObject[0]+" te ha invitado a su proyecto ¿Aceptas?");
+            if(irAProyecto){
+                redireccionar();
+            }
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient != null) {
+        $("#connect").prop("disabled", false);
+        $("#disconnect").prop("disabled", true);
+        stompClient.disconnect();
+    }
+    //setConnected(false);
+    console.log("Disconnected");
+}
+
+function redireccionar() {
+    disconnect();
+    window.location.href="proyecto.html";
+}
+
+function guardarNombre(){
+    var nombreProyecto = $("#nombreProyecto").val();
+    sessionStorage.nameProject = nombreProyecto;
+    redireccionar();
+}
+
 $(document).ready(
         function() {
-            $("#nombre").textContent = sessionStorage.name;
+            $("#nombre").html("<div class='container'>Esta logueado como "+sessionStorage.name+"</div>");
+            connect();            
         }
 );
