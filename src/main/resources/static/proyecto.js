4603abde6f829030431603bc636ce9872e8123ce
David Esteban Useche <david.useche@mail.escuelaing.edu.co>
2016-11-02 15:59:20 -0500

    Solo el dueño puede enviar invitaciones

--- src/main/resources/static/proyecto.js
+++ src/main/resources/static/proyecto.js
@@ -8,10 +8,10 @@ function connect() {
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
 
-        stompClient.subscribe('/topic/project.'+sessionStorage.nameProject, function (data) {
+        stompClient.subscribe('/topic/project.' + sessionStorage.nameProject, function (data) {
             var obj = JSON.parse(data.body);
             console.log(sessionStorage.name);
-            if(obj.author!=sessionStorage.name){
+            if (obj.author != sessionStorage.name) {
                 var patches = dmp.patch_fromText(obj.text);
                 text1 = $("#orig").val();
                 //cursor = editor.selection.getCursor();
@@ -19,35 +19,42 @@ function connect() {
                 var results = dmp.patch_apply(patches, text1);
                 console.log(patches);
                 $("#orig").val(results[0]);
-                editor.setValue(results[0],1);
-            }else{
+                editor.setValue(results[0], 1);
+            } else {
                 $("#orig").val(editor.getValue());
             }
         });
     });
 }
 
-function enviarInvitacion(){
-    var nombreInvitado= $("#Ncolaborador").val();
-    var miNombre = sessionStorage.name;
-    var nombreProy = sessionStorage.nameProject;
-    console.log(miNombre+" "+nombreProy);
-    stompClient.send("/topic/waiting."+nombreInvitado, {}, JSON.stringify({miNombre:miNombre,nombreProy:nombreProy}));
+function enviarInvitacion() {
+    $.get("intelijava/proyecto/" + sessionStorage.nameProject + "/duenno").then(function (data) {
+        if (data.nombre == sessionStorage.name) {
+            var nombreInvitado = $("#Ncolaborador").val();
+            var miNombre = sessionStorage.name;
+            var nombreProy = sessionStorage.nameProject;
+            console.log(miNombre + " " + nombreProy);
+            stompClient.send("/topic/waiting." + nombreInvitado, {}, JSON.stringify({miNombre: miNombre, nombreProy: nombreProy}));
+        }else{
+            alert("No eres el dueño de este proyecto");
+        }
+
+    });
 }
 
 function end() {
     var usuario = sessionStorage.name;
     var nombreProy = sessionStorage.nameProject;
-    $.post("/intelijava/proyecto/"+nombreProy+"/delcolaborador/",usuario, function(){
+    $.post("/intelijava/proyecto/" + nombreProy + "/delcolaborador/", usuario, function () {
         redireccionar();
         disconnect();
-    }).fail(function(){
+    }).fail(function () {
         alert("El usuario ya existe");
     });
 }
 
 function redireccionar() {
-    window.location.href="index.html";
+    window.location.href = "index.html";
 }
 
 function disconnect() {
@@ -76,7 +83,7 @@ $(document).ready(
                 var diff = dmp.diff_main(text1, text2, true);
                 var patch_list = dmp.patch_make(text1, text2, diff);
                 patch_text = dmp.patch_toText(patch_list);
-                stompClient.send("/topic/project."+sessionStorage.nameProject, {}, JSON.stringify({text: patch_text,author: sessionStorage.name}));
+                stompClient.send("/topic/project." + sessionStorage.nameProject, {}, JSON.stringify({text: patch_text, author: sessionStorage.name}));
             });
         }
 );
