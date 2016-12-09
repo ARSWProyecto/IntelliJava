fde9b6ac04f6ad9ef4f85697704f7a089db776c8
Alexander Ceron <edwin.ceron@hotmail.com>
2016-12-09 16:36:49 -0500

    arregalndo crear usuario

--- src/main/java/edu/eci/arsw/nieddu/intellijava/msgbroker/IntelijavaServicesRedis.java
+++ src/main/java/edu/eci/arsw/nieddu/intellijava/msgbroker/IntelijavaServicesRedis.java
@@ -47,7 +47,7 @@ public class IntelijavaServicesRedis implements Services{
         String resp = null;
         System.out.println("Entro al existe usuario");
         Jedis jedis = JedisUtil.getPool().getResource();
-        Map<String, String> usuario = jedis.hgetAll("nombre:" + nombre);
+        Map<String, String> usuario = jedis.hgetAll("usuario:" + nombre);
         System.out.println(usuario.size()+" "+usuario+" ");
         if (!usuario.isEmpty()) {
             resp = nombre;
