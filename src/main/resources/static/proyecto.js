2c31da26c93bbf399682202ced6bad8377c2afaf
Nicolás Gómez Solano <nicolasgomezsolano@yahoo.es>
2016-12-12 17:24:11 -0500

    Intentando arreglar el envío del texto

--- src/main/resources/static/proyecto.js
+++ src/main/resources/static/proyecto.js
@@ -13,19 +13,19 @@ function connect() {
             //console.log(sessionStorage.name);
             //console.log(data);
             /*obj = data.body.split("AUTOR");
-            author = obj[1];
-            text = obj[0];*/
+             author = obj[1];
+             text = obj[0];*/
             if (obj.author != sessionStorage.name) {
-            //if (author != sessionStorage.name) {
+                //if (author != sessionStorage.name) {
                 /*var patches = dmp.patch_fromText(obj.text);
-                text1 = $("#orig").val();
-                
-                //console.log(cursor);
-                var results = dmp.patch_apply(patches, text1);
-                //console.log(patches);
-                $("#orig").val(results[0]);
-                editor.setValue(results[0], 1);*/
-                
+                 text1 = $("#orig").val();
+                 
+                 //console.log(cursor);
+                 var results = dmp.patch_apply(patches, text1);
+                 //console.log(patches);
+                 $("#orig").val(results[0]);
+                 editor.setValue(results[0], 1);*/
+
                 editor.setValue(obj.text, 1);
                 //editor.setValue(text, 1);
                 col = parseInt(obj.col);
@@ -38,14 +38,15 @@ function connect() {
                 //console.log("myCol: "+myCol);
                 newCol = myCol;
                 newRow = myRow;
-                if(row==myRow){
-                    if(col<myCol){
+                if (row == myRow) {
+                    if (col < myCol) {
                         newCol++;
                     }
-                }if(row<=myRow){
+                }
+                if (row <= myRow) {
                     //console.log("Sí "+row+"<="+myRow);
-                    if(row_ant!=row){
-                        newRow+=(row-row_ant);
+                    if (row_ant != row) {
+                        newRow += (row - row_ant);
                     }
                 }
                 //console.log("Me tengo que mover a: "+newRow+", "+newCol);
@@ -54,15 +55,15 @@ function connect() {
                 sessionStorage.cursor_col = newCol;
             }
             /*else {
-                $("#orig").val(editor.getValue());
-            }*/
+             $("#orig").val(editor.getValue());
+             }*/
         });
         stompClient.subscribe('/topic/waitingBan.' + sessionStorage.nameProject, function (data) {
             //console.log(data);
             var theObject = JSON.parse(data.body);
             //console.log(theObject);
             var nombre = sessionStorage.name;
-            if (theObject.name == nombre){
+            if (theObject.name == nombre) {
                 alert("Has sido eliminado del proyecto");
                 desconectar();
             }
@@ -101,9 +102,9 @@ function borrarColaborador() {
                 }).fail(function (response) {
                     //console.log(response);
                     alert(response.responseText);
-                }).then(function(){
+                }).then(function () {
                     stompClient.send("/topic/waitingBan." + sessionStorage.nameProject, {}, JSON.stringify({name: nombreInvitado}));
-                    alert("Has baneado exitosamente a "+nombreInvitado);
+                    alert("Has baneado exitosamente a " + nombreInvitado);
                 });
             }
         } else {
@@ -154,28 +155,28 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-function compilar(){
-    $.get("intelijava/proyecto/" + sessionStorage.nameProject + "/colaborador/"+ sessionStorage.name + "/compilado").then(function (data) {
+function compilar() {
+    $.get("intelijava/proyecto/" + sessionStorage.nameProject + "/colaborador/" + sessionStorage.name + "/compilado").then(function (data) {
         $("#resCompilacion").html(data);
     });
 }
 
-function limpiarCompilacion(){
+function limpiarCompilacion() {
     $("#resCompilacion").html("Procesando... Por favor espere.");
 }
 
-function crearArbol(){
+function crearArbol() {
     $('#tree').jstree();
 }
 
 $(document).ready(
         function () {
-            if(sessionStorage.name==null || sessionStorage.nameProject==null){
+            if (sessionStorage.name == null || sessionStorage.nameProject == null) {
                 redireccionar();
             }
             crearArbol();
             $("#titulo").html(sessionStorage.nameProject);
-            $("#nombre_usuario").html("Hola, "+sessionStorage.name+" <span class='caret'></span>");
+            $("#nombre_usuario").html("Hola, " + sessionStorage.name + " <span class='caret'></span>");
             //$("#colaboran").html(sessionStorage.)
             connect();
             editor = ace.edit("text");
@@ -184,99 +185,43 @@ $(document).ready(
             editor.setTheme("ace/theme/monokai");
             sessionStorage.cursor_row = editor.getCursorPosition().row;
             sessionStorage.cursor_col = editor.getCursorPosition().column;
-            $('.ace_text-input').on('input keypress keydown', function () {
-            //editor.getSession().on('change',function(){
+            $('.ace_text-input').on('input selectionchange propertychange', function () {
+                //$('.ace_text-input').on('input keypress keydown', function () {
+                //editor.getSession().on('change',function(){
                 //stompClient.send("/topic/project." + sessionStorage.nameProject, {}, JSON.stringify({text: patch_text, author: sessionStorage.name}));*/
                 //stompClient.send("/app/project." + sessionStorage.nameProject, {}, editor.getValue() +"AUTOR"+sessionStorage.name);
                 stompClient.send("/app/project." + sessionStorage.nameProject, {}, JSON.stringify({text: editor.getValue(), author: sessionStorage.name, col: editor.getCursorPosition().column, row: editor.getCursorPosition().row, col_ant: sessionStorage.cursor_col, row_ant: sessionStorage.cursor_row}));
                 sessionStorage.cursor_row = editor.getCursorPosition().row;
                 sessionStorage.cursor_col = editor.getCursorPosition().column;
             });
-            
+
+            $('.ace_text-input').keyup(function (e) {
+                if (e.keyCode == 8) {
+                    stompClient.send("/app/project." + sessionStorage.nameProject, {}, JSON.stringify({text: editor.getValue(), author: sessionStorage.name, col: editor.getCursorPosition().column, row: editor.getCursorPosition().row, col_ant: sessionStorage.cursor_col, row_ant: sessionStorage.cursor_row}));
+                    sessionStorage.cursor_row = editor.getCursorPosition().row;
+                    sessionStorage.cursor_col = editor.getCursorPosition().column;
+                }
+            });
+
             $('#text').on('click', function () {
                 sessionStorage.cursor_row = editor.getCursorPosition().row;
                 sessionStorage.cursor_col = editor.getCursorPosition().column;
 
-	    //editor.getSession().on('change', function(){
+                //editor.getSession().on('change', function(){
                 /*text1 = $("#orig").val();
-                //text2 = $("#text2").val();
-                text2 = editor.getValue();
-                var diff = dmp.diff_main(text1, text2, true);
-                var patch_list = dmp.patch_make(text1, text2, diff);
-                patch_text = dmp.patch_toText(patch_list);
-                stompClient.send("/topic/project." + sessionStorage.nameProject, {}, JSON.stringify({text: patch_text, author: sessionStorage.name}));*/
+                 //text2 = $("#text2").val();
+                 text2 = editor.getValue();
+                 var diff = dmp.diff_main(text1, text2, true);
+                 var patch_list = dmp.patch_make(text1, text2, diff);
+                 patch_text = dmp.patch_toText(patch_list);
+                 stompClient.send("/topic/project." + sessionStorage.nameProject, {}, JSON.stringify({text: patch_text, author: sessionStorage.name}));*/
                 //sessionStorage.cursor = editor.getCursorPosition();
                 //alert(editor.getValue());
                 //stompClient.send("/app/project." + sessionStorage.nameProject, {}, editor.getValue() +"AUTOR"+sessionStorage.name);
             });
-            
-            $.get("intelijava/proyecto/"+sessionStorage.nameProject+"/paquete/0/archivo/0").then(function (data) {
-               editor.setValue(data, 1); 
+
+            $.get("intelijava/proyecto/" + sessionStorage.nameProject + "/paquete/0/archivo/0").then(function (data) {
+                editor.setValue(data, 1);
             });
         }
 );
-
-/*
- var dmp = new diff_match_patch();
- var Range = require("ace/range").Range;
- 
- function connect() {
- $("#connect").prop("disabled", true);
- $("#disconnect").prop("disabled", false);
- var socket = new SockJS('/stompendpoint');
- stompClient = Stomp.over(socket);
- stompClient.connect({}, function (frame) {
- console.log('Connected: ' + frame);
- 
- stompClient.subscribe('/topic/newpoint', function (data) {
- doc = editor.getSession().getDocument();
- var patch_text = JSON.parse(data.body);
- diff = patch_text.text;
- var offset = 0;
- diff.forEach(function (chunk) {
- var op = chunk[0];
- var text = chunk[1];
- 
- if (op === 0) {
- offset += text.length;
- } else if (op === -1) {
- doc.remove(Range.fromPoints(
- doc.indexToPosition(offset),
- doc.indexToPosition(offset + text.length)
- ));
- } else if (op === 1) {
- doc.insert(doc.indexToPosition(offset), text);
- offset += text.length;
- }
- });
- $("#orig").val(editor.getValue());
- });
- });
- }
- 
- function disconnect() {
- if (stompClient != null) {
- $("#connect").prop("disabled", false);
- $("#disconnect").prop("disabled", true);
- stompClient.disconnect();
- }
- //setConnected(false);
- console.log("Disconnected");
- }
- 
- $(document).ready(
- function () {
- connect();
- editor = ace.edit("text");
- editor.getSession().setMode("ace/mode/python");
- editor.setTheme("ace/theme/monokai");
- $('#text').on('input selectionchange propertychange', function () {
- text1 = $("#orig").val();
- //text2 = $("#text2").val();
- text2 = editor.getValue();
- var diff = dmp.diff_main(text1, text2, true);
- stompClient.send("/app/newpoint", {}, JSON.stringify({text: diff}));
- });
- }
- );
- */
